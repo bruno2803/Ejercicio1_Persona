5a9bcd96bab43aa223dc6ed47d506d4e41e9d522
bruno2803 <104646736+bruno2803@users.noreply.github.com>
2023-08-30 17:16:23 -0300

    Add files via upload

---
+++ src/main/java/com/utn/entidades/Persona.java
@@ -0,0 +1,28 @@
+package com.utn.ejerc01.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+}
